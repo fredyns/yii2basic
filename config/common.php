4b77fa6a13e93f3112357064f8b0fba73520e41b
Fredy Nurman Saleh <email@fredyns.net>
2019-07-11 11:29:47 +0700

    config for both web & console

---
+++ config/common.php
@@ -0,0 +1,35 @@
+<?php
+$db = require __DIR__.'/db.php';
+$mailer = require __DIR__.'/mailer.php';
+$params = require __DIR__.'/params.php';
+
+$config = [
+    'id' => 'basic_app',
+    'name' => 'My Web-Application',
+    'timeZone' => 'Asia/Jakarta',
+    'language' => 'id-ID',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+        '@tests' => '@app/tests',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'mailer' => $mailer,
+    ],
+    'params' => $params,
+];
+
+return $config;
