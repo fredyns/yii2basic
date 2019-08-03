238135c7127e364f7b120bc0b5444119a2c52a62
Fredy Nurman Saleh <email@fredyns.net>
2019-08-03 16:32:55 +0700

    fix ':' typo

--- generators/crud/default/views/archive.php
+++ generators/crud/default/views/archive.php
@@ -95,7 +95,7 @@ $this->params['breadcrumbs'][] = <?=$generator->generateString('Archives')?>;
             'headerRowOptions' => ['class' => 'x'],
             'columns' => [
                 [
-                    'class' => \kartik\grid\SerialColumn:class,
+                    'class' => \kartik\grid\SerialColumn::class,
                 ],
 <?php $count = 0; ?>
 <?php foreach ($safeAttributes as $attribute): ?>
