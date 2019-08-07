9c515412b6f7ed06167b9d0896ad3835f2a155b0
Fredy Nurman Saleh <email@fredyns.net>
2019-08-07 09:45:32 +0700

    WIP: model menu

---
+++ generators/crud/default/ModelMenu.php
@@ -0,0 +1,60 @@
+<?php
+use yii\db\Schema;
+use yii\helpers\StringHelper;
+use yii\helpers\Inflector;
+
+/* @var $this \yii\web\View  */
+/* @var $generator \app\generators\crud\Generator  */
+/* @var $tableSchema \yii\db\TableSchema  */
+/* @var $giiConfigs array  */
+/* @var $softdelete bool  */
+/* @var $model \yii\db\ActiveRecord  */
+/* @var $controllerClassName string  */
+/* @var $controllerNameSpace string  */
+/* @var $moduleNameSpace string  */
+/* @var $subPath string  */
+/* @var $actionParentNameSpace string  */
+/* @var $actionParent string[]  */
+/* @var $dateRange string[]  */
+/* @var $timestampRange string[]  */
+
+$modelClassName = StringHelper::basename($generator->modelClass);
+$route = ($subPath ? "/{$subPath}/" : "/")
+    .Inflector::camel2id(str_replace('Controller', '', $controllerClassName));
+
+echo '<?php\n';
+?>
+
+namespace <?= StringHelper::dirname($generator->searchModelClass) ?>;
+
+/**
+ * Action menu manager for model <?= $modelClassName ?>
+ *
+ * @author Fredy Nurman Saleh <email@fredyns.net>
+ */
+class <?= $modelClassName ?>Menu extends \app\lib\ModelMenu
+{
+    public static $controller = '<?= $route ?>';
+    public static $softdelete = <?= $tableSchema->getColumn('is_deleted') !== NUll ? 'true' : 'false' ?>;
+
+    public static function actionControls()
+    {
+        $classes = [
+            static::INDEX => \<?= $actionParentNameSpace ?>\index\ActionControl::class,
+            static::VIEW => \<?= $actionParentNameSpace ?>\view\ActionControl::class,
+            static::CREATE => \<?= $actionParentNameSpace ?>\create\ActionControl::class,
+            static::UPDATE => \<?= $actionParentNameSpace ?>\update\ActionControl::class,
+            static::DELETE => \<?= $actionParentNameSpace ?>\delete\ActionControl::class,
+<?php if ($tableSchema->getColumn('is_deleted') !== NUll): ?>
+            static::DELETED => \<?= $actionParentNameSpace ?>\deleted\ActionControl::class,
+            static::RESTORE => \<?= $actionParentNameSpace ?>\restore\ActionControl::class,
+            static::ARCHIVE => \<?= $actionParentNameSpace ?>\archive\ActionControl::class,
+<?php endif; ?>
+        ];
+        // remove this line to disable default action controls
+        $classes = parent::actionControls();
+
+        return $classes;
+    }
+
+}
