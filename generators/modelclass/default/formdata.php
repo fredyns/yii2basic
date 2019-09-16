669ec62dd3042b3029553fb31f3416bfd0fb46ab
Fredy Nurman Saleh <email@fredyns.net>
2019-09-16 21:34:57 +0700

    decide message category

--- generators/modelclass/default/formdata.php
+++ generators/modelclass/default/formdata.php
@@ -6,7 +6,22 @@ use yii\helpers\Inflector;
 /* @var $generator app\generators\model2\Generator  */
 /* @var $tableName string  full table name */
 /* @var $modelClass string  */
-/* @var $ns string  */
+/* @var $ns string model's namespace */
+/**
+ * namespace pattern:
+ * app/models/sub
+ * app/modules/moduleId/models/sub
+ */
+$nameSpaceArray = explode("\\", str_replace("\\\\", "\\", $ns));
+if (isset($nameSpaceArray[1]) && $nameSpaceArray[1] == 'modules' && isset($nameSpaceArray[2])) {
+    $moduleId = $nameSpaceArray[2];
+    $subPath = isset($nameSpaceArray[4]) ? $nameSpaceArray[4] : null;
+} else {
+    $moduleId = 'app';
+    $subPath = isset($nameSpaceArray[2]) ? $nameSpaceArray[2] : null;
+}
+
+$messageCategory = trim("{$moduleId}/{$subPath}/models", "/");
 ?>
 {
     "tablename": {
@@ -38,7 +53,7 @@ use yii\helpers\Inflector;
         "name": "generateRelations"
     },
     "messagecategory": {
-        "value": "models",
+        "value": "<?= $messageCategory ?>",
         "name": "messageCategory"
     }
 }
