9c7130880c167f2b0d6740b2e9298e7c3798c7d7
Fredy Nurman Saleh <email@fredyns.net>
2020-03-30 08:58:19 +0700

    remark non existing migration

--- runcloud-deployment.sh
+++ runcloud-deployment.sh
@@ -4,5 +4,5 @@ git pull origin master
 composer install
 
 ./yii migrate --interactive=0 --migration-path=@vendor/dektrium/yii2-user/migrations
-./yii migrate --interactive=0 --migration-path=@mdm/upload/migrations
+#./yii migrate --interactive=0 --migration-path=@mdm/upload/migrations # not exist yet
 ./yii migrate --interactive=0
