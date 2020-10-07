b1f97dc46dc10b5ce3cf425b0dc2b459d4c50b18
Fredy Nurman Saleh <email@fredyns.net>
2020-10-07 09:45:29 +0700

    move to PgSQL

--- config/db.php
+++ config/db.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * this file responsible for database connection used
+ *
+ * sample & doc can be found at https://www.yiiframework.com/doc/guide/2.0/en/db-dao#creating-db-connections
  */
 if (file_exists(__DIR__.'/host/db.php')) {
     // use host configuration if any
@@ -9,8 +11,8 @@ if (file_exists(__DIR__.'/host/db.php')) {
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=yii2basic',
+    'username' => 'fredy',
     'password' => '',
     'charset' => 'utf8',
     // Schema cache options (for production environment)
